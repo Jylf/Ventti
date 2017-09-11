8bde8026b8cdc8c39a1061ae6c778e361c21bd42
Jylf <31842206+Jylf@users.noreply.github.com>
2017-09-11 12:16:20 +0300

    Testi1

--- src/ventti/Ventti.java
+++ src/ventti/Ventti.java
@@ -21,32 +21,32 @@ public class Ventti {
 		List<String> kortit = new ArrayList<String>();
 		
 		
-		kortit.add("2");
-		kortit.add("3");
-		kortit.add("4");
-                kortit.add("5");
-                kortit.add("6");
-                kortit.add("7");
-                kortit.add("8");
-                kortit.add("9");
-                kortit.add("10");
-                kortit.add("11");
-                kortit.add("12");
-                kortit.add("13");
-                kortit.add("14");
-                kortit.add("2");
-		kortit.add("3");
-		kortit.add("4");
-                kortit.add("5");
-                kortit.add("6");
-                kortit.add("7");
-                kortit.add("8");
-                kortit.add("9");
-                kortit.add("10");
-                kortit.add("11");
-                kortit.add("12");
-                kortit.add("13");
-                kortit.add("14");
+		kortit.add("Pata2");
+		kortit.add("Pata3");
+		kortit.add("Pata4");
+                kortit.add("Pata5");
+                kortit.add("Pata6");
+                kortit.add("Pata7");
+                kortit.add("Pata8");
+                kortit.add("Pata9");
+                kortit.add("Pata10");
+                kortit.add("Pata11");
+                kortit.add("Pata12");
+                kortit.add("Pata13");
+                kortit.add("Pata14");
+                kortit.add("Ruutu2");
+		kortit.add("Ruutu3");
+		kortit.add("Ruutu4");
+                kortit.add("Ruutu5");
+                kortit.add("Ruutu6");
+                kortit.add("Ruutu7");
+                kortit.add("Ruutu8");
+                kortit.add("Ruutu9");
+                kortit.add("Ruutu10");
+                kortit.add("Ruutu11");
+                kortit.add("Ruutu12");
+                kortit.add("Ruutu13");
+                kortit.add("Ruutu14");
                 kortit.add("2");
 		kortit.add("3");
 		kortit.add("4");
@@ -90,3 +90,20 @@ public class Ventti {
     }
     
 }
+
+
+//** Generate 10 random integers in the range 0..99. */
+//public final class RandomInteger {
+  
+  //public static final void main(String... aArgs){
+    //log("Generating 10 random integers in range 0..99.");
+    
+    //note a single Random object is reused here
+    //Random randomGenerator = new Random();
+    //for (int idx = 1; idx <= 10; ++idx){
+    //  int randomInt = randomGenerator.nextInt(100);
+    //  log("Generated : " + randomInt);
+   // }
+    
+    //log("Done.");
+ // }
